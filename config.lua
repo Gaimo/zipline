d441d1a13df960aba586ced3e7f6a1076b56b508
Guilherme <41448137+Gaimo@users.noreply.github.com>
2024-03-12 13:59:32 -0300

    Added ziplineVelocity

--- config.lua
+++ config.lua
@@ -1,11 +1,12 @@
 config = {
     ziplineColRadius = 1,
+    ziplineVelocity = 30,
 
     ziplines = {
         -- test 1
         {
             a = {112.39542, -53.49337, 1.57812},
-            b = {138.62656, -45.75182, 10.15259}
+            b = {138.62656, -45.75182, 10.85259}
         }
     }
 
@@ -13,4 +14,4 @@ config = {
 
 --DEV
 setDevelopmentMode (true)
-print(showCol(true))
+showCol(true)
